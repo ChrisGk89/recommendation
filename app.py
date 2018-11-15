9a95b045778ac33f14febf65bfddc1858680ae5e
Chris <info@chrisgkalfas.com>
2018-11-15 08:46:43 +0200

    Significant Commit
    Many changes in templates and routes.

--- app.py
+++ app.py
@@ -1,15 +1,9 @@
 from flask import Flask, request, render_template, jsonify
 from flask_restful import Resource, Api
+from typing import List, Any, Tuple
 from math import sqrt
-
-
+import requests
 from sqlalchemy import create_engine
-import numpy as np
-from sklearn import preprocessing, neighbors
-import pandas as pd
-from json import dumps
-from flask.ext.jsonpify import jsonify
-
 
 ratings = {'Lisa': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.5,
                     'Just My Luck': 3.0, 'Superman Returns': 3.5, 'You, Me and Dupree': 2.5,
@@ -29,23 +23,15 @@ ratings = {'Lisa': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.5,
                     'The Night Listener': 3.0, 'Superman Returns': 5.0, 'You, Me and Dupree': 3.5},
            'Toby': {'Snakes on a Plane': 4.5, 'You, Me and Dupree': 1.0, 'Superman Returns': 4.0}}
 
-# df = pd.read_csv('')
 
-db_connect = create_engine('sqlite:///chinook.db')
 app = Flask(__name__)
 api = Api(app)
 
 
-@app.route('/')
-def main():
-    return render_template('main.html')
+#USER BASED COLLABORATIVE FILTER
 
 
-if __name__ == '__main__':
-    app.run()
-
-
-# Returns a distance-based similarity score for person1 and person2
+# Returns an Euclidean distance similarity score between person1 and person2
 def sim_distance(prefs, person1, person2):
     # Get the list of shared_items
     si = {}
@@ -53,35 +39,44 @@ def sim_distance(prefs, person1, person2):
         if item in prefs[person2]:
             si[item] = 1
     # if they have no ratings in common, return 0
-    if len(si) == 0: return 0
-    # Add up the squares of all the differences
+    if len(si) == 0:
+        return 0
+    # Add the squares of all the differences
     sum_of_squares = sum([pow(prefs[person1][item] - prefs[person2][item], 2)
                           for item in prefs[person1] if item in prefs[person2]])
     return 1 / (1 + sum_of_squares)
 
 
-# Returns the Pearson correlation coefficient for p1 and p2
-def sim_pearson(prefs, p1, p2):
+# Returns the Pearson correlation coefficient between person1 and person2
+def sim_pearson(prefs, person1, person2):
     # Get the list of mutually rated items
     si = {}
-    for item in prefs[p1]:
-        if item in prefs[p2]: si[item] = 1
-    # Find the number of elements
+    for item in prefs[person1]:
+        if item in prefs[person2]:
+            si[item] = 1
+
+    # Find the number of elements of the ratings dictionary
     n = len(si)
+
     # if they are no ratings in common, return 0
     if n == 0:
         return 0
-    # Add up all the preferences
-    sum1 = sum([prefs[p1][it] for it in si])
-    sum2 = sum([prefs[p2][it] for it in si])
-    # Sum up the squares
-    sum1Sq = sum([pow(prefs[p1][it], 2) for it in si])
-    sum2Sq = sum([pow(prefs[p2][it], 2) for it in si])
-    # Sum up the products
-    pSum = sum([prefs[p1][it] * prefs[p2][it] for it in si])
-    # Calculate Pearson score
+
+    # Add up all the preferences from ratings
+    sum1 = sum([prefs[person1][it] for it in si])
+    sum2 = sum([prefs[person2][it] for it in si])
+
+    # Sum of the squares
+    sum1Sq = sum([pow(prefs[person1][it], 2) for it in si])
+    sum2Sq = sum([pow(prefs[person2][it], 2) for it in si])
+
+    # Sum of the products
+    pSum = sum([prefs[person1][it] * prefs[person2][it] for it in si])
+
+    # Calculate r Pearson score
     num = pSum - (sum1 * sum2 / n)
     den = sqrt((sum1Sq - pow(sum1, 2) / n) * (sum2Sq - pow(sum2, 2) / n))
+
     if den == 0:
         return 0
 
@@ -90,35 +85,54 @@ def sim_pearson(prefs, p1, p2):
 
 
 # Returns the best matches for person from the prefs dictionary.
-# Number of results and similarity function are optional params.
-def topMatches(prefs, person, n=5, similarity=sim_pearson):
+#Find the top matches using Euclidean distance as similarity measure
+def topMatches(prefs, person, n=3, similarity=sim_distance):
     scores = [(similarity(prefs, person, other), other)
               for other in prefs if other != person]
-    # Sort the list so the highest scores appear at the top scores.sort()
+    # Sort the list so the highest scores appear at the top
+    scores.sort()
     scores.reverse()
     return scores[0:n]
 
 
-# Gets recommendations for a person by using a weighted average
+# Returns the best matches for person from the prefs dictionary.
+# Number of results and similarity function are optional params.
+#In our case the similarity measure and number of results is fixed
+# to Pearson correlation coefficient and 3 respectively
+def topMatches2(prefs, person, n=3, similarity=sim_pearson):
+    scores = [(similarity(prefs, person, other), other)
+              for other in prefs if other != person]
+    # Sort the list so the highest scores appear at the top
+    scores.sort()
+    scores.reverse()
+    return scores[0:n]
+
+
+# Gets recommendations for a person using a weighted average
 # of every other user's rankings
-def getRecommendations(prefs, person, similarity=sim_pearson):
+def getRecommendations(prefs, person, similarity):
     totals = {}
     simSums = {}
-    for other in prefs:
+
+    for other in prefs:  # type: object
         # don't compare me to myself
-        if other == person: continue
+        if other == person:
+            continue
         sim = similarity(prefs, person, other)
+
         # ignore scores of zero or lower
-        if sim <= 0: continue
+        if sim <= 0:
+            continue
         for item in prefs[other]:
+
             # only score movies I haven't seen yet
             if item not in prefs[person] or prefs[person][item] == 0:
                 # Similarity * Score
                 totals.setdefault(item, 0)
                 totals[item] += prefs[other][item] * sim
-        # Sum of similarities
-        simSums.setdefault(item, 0)
-        simSums[item] += sim
+                # Sum of similarities
+                simSums.setdefault(item, 0)
+                simSums[item] += sim
 
     # Create the normalized list
     rankings = [(total / simSums[item], item) for item, total in totals.items()]
@@ -128,8 +142,108 @@ def getRecommendations(prefs, person, similarity=sim_pearson):
     rankings.reverse()
     return rankings
 
-# @app.route('/recommendation/api/v1.0/ratings', methods=['GET'])
-# def get_ratings():
-#    return jsonify({'ratings': ratings})
-# if __name__ == '__main__':
-#    app.run()
+
+# ITEM BASED FILTERING
+
+
+#This function transform the data from user based to item based
+#in our case we group the data using the names of the movies
+#instead the name of users.
+def transform_prefs(prefs):
+    result = {}
+    for person in prefs:
+        for item in prefs[person]:
+            result.setdefault(item, {})
+            # Flip item and person
+            result[item][person] = prefs[person][item]
+    return result
+
+
+def calculate_similar_items(prefs, n=10):
+    # Create a dictionary of items showing which other items they
+    # are most similar to.
+    result = {}
+    # Invert the preference matrix to be item-centric
+    itemPrefs = transform_prefs(prefs)
+    c = 0
+    for item in itemPrefs:
+        # Status updates for large datasets
+        c += 1
+        if c % 100 == 0:
+            print "%d / %d" % (c, len(itemPrefs))
+        # Find the most similar items to this one
+        scores = topMatches(itemPrefs, item, n=n, similarity=sim_distance)
+        result[item] = scores
+    return result
+
+
+#To run this function correctly we first need to transform the data
+#using the transform preps then calculate similar items and finally
+#run this function
+def get_recommended_items(prefs, itemMatch, user):
+    userRatings = prefs[user]
+    scores = {}
+    totalSim = {}
+
+    # Loop over items rated by this user
+    for (item, rating) in userRatings.items():
+        # Loop over items similar to this one
+        for (similarity, item2) in itemMatch[item]:
+            # Ignore if this user has already rated this item
+            if item2 in userRatings:
+                continue
+            # Weighted sum of rating times similarity
+            scores.setdefault(item2, 0)
+            scores[item2] += similarity * rating
+            # Sum of all the similarities
+            totalSim.setdefault(item2, 0)
+            totalSim[item2] += similarity
+        # Divide each total score by total weighting to get an average
+        rankings = [(score / totalSim[item], item) for item, score in scores.items()]
+        # Return the rankings from highest to lowest
+    rankings.sort()
+    rankings.reverse()
+    return rankings
+
+
+@app.route('/findpeople', methods=['GET'])
+def get_rmatches():
+    res = topMatches2(ratings, "Toby", sim_pearson())
+    return jsonify(res)
+
+
+@app.route('/euclidean', methods=['GET'])
+def get_euclidean():
+    result = getRecommendations(ratings, "Toby", sim_distance)
+    return jsonify(result)
+
+
+@app.route('/pearson', methods=['GET'])
+def get_pearson():
+    result2 = getRecommendations(ratings, "Toby", sim_pearson)
+    return jsonify(result2)
+
+
+@app.route('/')
+def main():
+    return render_template('main.html')
+
+
+@app.route('/userbased')
+def main2():
+    return render_template('userbased.html')
+
+
+@app.route('/itembased')
+def main3():
+    return render_template('itembased.html')
+
+
+
+#@app.route('/itembased')
+#def main3():
+#    return
+
+
+if __name__ == '__main__':
+    app.run()
